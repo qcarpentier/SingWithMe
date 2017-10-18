d18c5c75620a86621d1d71a16fd744b347311d61
Carpentier Quentin <quentcarp@hotmail.com>
2017-10-18 22:50:29 +0200

    Add project files.

---
+++ SingWithMe/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SingWithMe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
